b899ab3fd4e44cefd2322dc11a886a7ea385c05a
ZhekaGrushevskiy <75371987+ZhekaGrushevskiy@users.noreply.github.com>
2021-02-27 22:15:33 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$('#items li').swipe(function(){
-  $('.delete').hide();
-  $('.delete', this).show();
-})
-
-// delete row on tapping delete button
-$('.delete').tap(function(){
-  $(this).parent('li').remove();
-})
